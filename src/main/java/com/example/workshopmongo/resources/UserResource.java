b19bc8bf3f5495687b006bc88e12b79daf3a2d4c
Nildo Tolentino <nildotrodrigues@gmail.com>
2024-02-27 16:33:59 -0300

    conectando ao MongoDB com Serice e repository

--- src/main/java/com/example/workshopmongo/resources/UserResource.java
+++ src/main/java/com/example/workshopmongo/resources/UserResource.java
@@ -4,25 +4,24 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.workshopmongo.domain.User;
+import com.example.workshopmongo.domain.services.UserService;
 
 @RestController
 @RequestMapping(value="/users")
 public class UserResource {
-
+    @Autowired
+	private UserService service;
     /*Opcional GetMapping*/
 	@RequestMapping(method = RequestMethod.GET)
 	public ResponseEntity< List<User>> buscaTodosUser() {
-	
-		User usr1 = new User("1", "Maria Silva", "maria@gmail.com");
-		User usr2 = new User("2", "Alex Silva", "alex@gmail.com");
-		List<User> list = new ArrayList<>(); 
-		list.addAll(Arrays.asList(usr1,usr2));
+		List<User> list = service.findAll();
 		return ResponseEntity.ok().body(list);
 		
 	};
