2114338abc44bf2ac920e965e22b84825c90b47a
Nildo Tolentino <nildotrodrigues@gmail.com>
2024-02-28 14:46:12 -0300

    excluindo usuario com delete

--- src/main/java/com/example/workshopmongo/domain/services/UserService.java
+++ src/main/java/com/example/workshopmongo/domain/services/UserService.java
@@ -33,6 +33,13 @@ public class UserService {
     	
     }
     
+    
+    public void delete(String id) {
+    	findById(id);
+    	repo.deleteById(id);
+    }
+    
+    
     public User fromDTO(UserDTO objdto) {
     	
     	return new User(objdto.getId(), objdto.getName(), objdto.getEmail());
