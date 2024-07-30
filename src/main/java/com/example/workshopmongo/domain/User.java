ddd28ff3dc2d61c439c51063d47b9f4e398fa4d2
Nildo Tolentino <nildotrodrigues@gmail.com>
2024-07-30 14:24:44 -0300

    Guardando ponteiro post na classe user (metodo aninhado)

--- src/main/java/com/example/workshopmongo/domain/User.java
+++ src/main/java/com/example/workshopmongo/domain/User.java
@@ -1,7 +1,11 @@
 package com.example.workshopmongo.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
+
+import org.springframework.data.mongodb.core.mapping.DBRef;
 
 public class User implements Serializable {
 	private static final long serialVersionUID = 1L;
@@ -9,6 +13,9 @@ public class User implements Serializable {
 	private String id;
 	private String name;
 	private String email;
+	
+	@DBRef(lazy=true)
+	private List<Post> posts  = new ArrayList<>();
 	
 	public User() {
 	}
@@ -43,6 +50,14 @@ public class User implements Serializable {
 	public void setEmail(String email) {
 		this.email = email;
 	}
+	
+	public List<Post> getPosts() {
+		return posts;
+	}
+
+	public void setPosts(List<Post> posts) {
+		this.posts = posts;
+	};
 
 	@Override
 	public int hashCode() {
@@ -59,7 +74,9 @@ public class User implements Serializable {
 			return false;
 		User other = (User) obj;
 		return Objects.equals(id, other.id);
-	};
+	}
+
+	
 	
 	
 	
