971e0b2517764e746a02aa94dd43c6703e769b95
Nildo Tolentino <nildotrodrigues@gmail.com>
2024-07-31 09:48:16 -0300

    Consulta usando @Query

--- src/main/java/com/example/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/example/workshopmongo/repository/PostRepository.java
@@ -3,12 +3,18 @@ package com.example.workshopmongo.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.example.workshopmongo.domain.Post;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post,String>{
+	
+	@Query("{ 'title': {$regex: ?0 , $options:'i' }  }" )
+	/* ?0 eh o primeiro parametro abaixo, ou seja o  --> text*/
+	/* $options:'i'  parametro para case sensitive */
+	List<Post> searchTitle(String text);
 	
 	List<Post> findByTitleContainingIgnoreCase(String text );
 
